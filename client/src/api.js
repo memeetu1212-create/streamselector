8eb77a4ee1f0477605a34082c93de6c9e3c552c6
memeetu1212-create <memeetu1212@gmail.com>
2026-04-17 12:13:37 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://streamselector-api.onrender.com/api";
+const API_BASE_URL = "https://streamselector.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL
